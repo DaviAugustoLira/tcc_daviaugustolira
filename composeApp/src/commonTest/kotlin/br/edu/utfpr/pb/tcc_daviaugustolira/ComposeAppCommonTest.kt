bd43e488585ea5173be8d8247b5bdde7f480a0bd
DaviAugustoLira <daviaugustolira@gmail.com>
2026-04-25 17:07:32 -0300

    initial commit

---
+++ composeApp/src/commonTest/kotlin/br/edu/utfpr/pb/tcc_daviaugustolira/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package br.edu.utfpr.pb.tcc_daviaugustolira
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/br/edu/utfpr/pb/tcc_daviaugustolira/MainViewController.kt
@@ -0,0 +1,5 @@
+package br.edu.utfpr.pb.tcc_daviaugustolira
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
